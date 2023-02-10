e01ab4bb22e45b7fae49341e9e25a3315afe832d
Samoeunchh <samoeunchh@gmail.com>
2023-02-10 10:56:25 +0700

    Item with uplods image

--- SS12CarWashing/Controllers/ItemsController.cs
+++ SS12CarWashing/Controllers/ItemsController.cs
@@ -12,10 +12,12 @@ namespace SS12CarWashing.Controllers
     public class ItemsController : Controller
     {
         private readonly AppDbContext _context;
-
-        public ItemsController(AppDbContext context)
+        private readonly IWebHostEnvironment webHost;
+        public ItemsController(AppDbContext context, 
+            IWebHostEnvironment webHost)
         {
             _context = context;
+            this.webHost = webHost;
         }
 
         // GET: Items
@@ -59,19 +61,49 @@ namespace SS12CarWashing.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ItemId,ItemTypeId,ItemName,Price,BrandId,ModelId,IsStock,QtyOnHand,OilTypeId,Size,Image")] Item item)
+        public async Task<IActionResult> Create(ItemDTO item)
         {
             if (ModelState.IsValid)
             {
-                item.ItemId = Guid.NewGuid();
-                _context.Add(item);
+                var itemDto = new Item
+                {
+                    OilTypeId= item.OilTypeId,
+                    ItemId= Guid.NewGuid(),
+                    IsStock=item.IsStock,
+                    ItemName=item.ItemName,
+                    ItemTypeId= item.ItemTypeId,
+                    BrandId= item.BrandId,
+                    ModelId= item.ModelId,
+                    Price= item.Price,
+                    QtyOnHand=item.QtyOnHand,
+                    Size= item.Size,
+                    Image=UploadFile(item)
+                };
+                _context.Add(itemDto);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
             ViewData["ItemTypeId"] = new SelectList(_context.ItemType, "ItemTypeId", "ItemTypeName", item.ItemTypeId);
+            ViewData["Brands"] = new SelectList(_context.Brand, "BrandId", "BrandName",item.BrandId);
+            ViewData["Models"] = new SelectList(_context.Models, "ModelId", "ModelName",item.ModelId);
+            ViewData["OilTypes"] = new SelectList(_context.OilType, "OilTypeId", "OilTypeName",item.OilTypeId);
             return View(item);
         }
-
+        private string UploadFile(ItemDTO item)
+        {
+            string path = "";
+            if(item.Image != null)
+            {
+                var root = webHost.WebRootPath + "/Uploads/";
+                path = item.Image.FileName;
+                var fullPath = Path.Combine(root, path);
+                using(FileStream fs=new FileStream(fullPath,FileMode.Create))
+                {
+                    item.Image.CopyTo(fs);
+                }
+            }
+            return path;
+        }
         // GET: Items/Edit/5
         public async Task<IActionResult> Edit(Guid? id)
         {
---
+++ SS12CarWashing/Models/ItemDTO.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace SS12CarWashing.Models
+{
+    public class ItemDTO
+    {
+        [Key]
+        public Guid ItemId { get; set; }
+        [ForeignKey("ItemType")]
+        [Display(Name = "Item Type")]
+        public Guid ItemTypeId { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Item Name")]
+        public string ItemName { get; set; }
+        public double Price { get; set; }
+        [Display(Name = "Brand Name")]
+        public string BrandId { get; set; }
+        [Display(Name = "Model")]
+        public string ModelId { get; set; }
+        public bool IsStock { get; set; }
+        [Display(Name = "Qty Stock")]
+        public int QtyOnHand { get; set; }
+        [Display(Name = "Oil Type")]
+        public string OilTypeId { get; set; }
+        public string Size { get; set; }
+        public IFormFile Image { get; set; }
+        public ItemType ItemType { get; set; }
+    }
+}
